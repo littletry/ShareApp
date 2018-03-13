a0149f13a2d3b512738dabe5869c550ef8e40cbc
LittleTry <ht@yuancore.com>
2018-03-13 16:26:39 +0800

    添加代码检查功能的插件，并添加一些本机调试的示例代码

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,10 @@
+// 是否禁用代理
+const noProxy = process.env.NO_PROXY === 'false';
 
-export default {
+// 代码中会兼容本地 service mock 以及部署站点的静态数据
+const proxy = {
+  /* eslint-disable */
+  '/vcs/api/client/details': 'http://192.168.0.66:7080',
 };
+
+export default noProxy ? {} : delay(proxy, 1000);
