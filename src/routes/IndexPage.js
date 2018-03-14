4cc094d7c79552a1c192a58e3c2672973a6d39c7
LittleTry <ht@yuancore.com>
2018-03-15 01:22:40 +0800

    登录页面有点儿像样了

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Toast, WingBlank, List, InputItem, WhiteSpace, Button } from 'antd-mobile';
+import { createForm } from 'rc-form';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+
+@connect(state => ({
+  login: state.login,
+}))
+
+class IndexPage extends Component {
+  componentDidMount() {
+    Toast.loading('加载中', 2);
+  }
+  render() {
+    const { getFieldProps } = this.props.form;
+    return (
+      <div>
+        <WingBlank size="md">
+          <div className={styles.placeholder}>欢迎进入分享空间</div>
+        </WingBlank>
+        <List>
+          <WhiteSpace size="lg" />
+          <InputItem
+            {...getFieldProps('username')}
+            placeholder="请输入用户名"
+            lableNumber="7"
+          />
+          <WhiteSpace size="lg" />
+          <InputItem
+            {...getFieldProps('password')}
+            placeholder="请输入密码"
+            lableNumber="7"
+          />
+          <WhiteSpace size="lg" />
+          <Button type="primary" inline style={{ width: '40%', marginLeft: '6%', marginRight: '4%' }} >登 录</Button>
+          <Button type="primary" inline style={{ width: '40%', marginLeft: '4%', marginRight: '6%' }} >注 册</Button>
+        </List>
+      </div>
+    );
+  }
 }
-
-IndexPage.propTypes = {
-};
-
-export default connect()(IndexPage);
+const IndexPages = createForm()(IndexPage);
+export default connect()(IndexPages);
