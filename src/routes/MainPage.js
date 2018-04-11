8a74df10a3cb96c5e45df94e2364e50b900ec0d1
LittleTry <ht@yuancore.com>
2018-04-11 17:41:11 +0800

    打包后不能显示svg图片，改为css样式

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { TabBar } from 'antd-mobile';
+import { TabBar, Icon } from 'antd-mobile';
 import { createForm } from 'rc-form';
 import { connect } from 'dva';
+import styles from './MainPage.css';
 
 @connect(state => ({
   main: state.main,
@@ -31,18 +32,8 @@ class MainPage extends Component {
           <TabBar.Item
             title="所有分享"
             key="all"
-            icon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/all.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
-            selectedIcon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/all_select.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
+            icon={<Icon className={styles.all} />}
+            selectedIcon={<Icon className={styles.all_select} />}
             selected={this.state.selectedTab === 'allTab'}
             onPress={() => {
               this.setState({
@@ -56,18 +47,8 @@ class MainPage extends Component {
           <TabBar.Item
             title="我的分享"
             key="mine"
-            icon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/mine.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
-            selectedIcon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/mine_select.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
+            icon={<Icon className={styles.mine} />}
+            selectedIcon={<Icon className={styles.mine_select} />}
             selected={this.state.selectedTab === 'mineTab'}
             onPress={() => {
               this.setState({
@@ -81,18 +62,8 @@ class MainPage extends Component {
           <TabBar.Item
             title="分享"
             key="share"
-            icon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/share.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
-            selectedIcon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/share_select.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
+            icon={<Icon className={styles.share} />}
+            selectedIcon={<Icon className={styles.share_select} />}
             selected={this.state.selectedTab === 'shareTab'}
             onPress={() => {
               this.setState({
@@ -106,18 +77,8 @@ class MainPage extends Component {
           <TabBar.Item
             title="我的"
             key="my"
-            icon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/my.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
-            selectedIcon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(src/assets/my_select.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
+            icon={<Icon className={styles.my} />}
+            selectedIcon={<Icon className={styles.my_select} />}
             selected={this.state.selectedTab === 'myTab'}
             onPress={() => {
               this.setState({
