078cb71ce1797cb251bac27ffcb3fc66b4ce41bf
LittleTry <ht@yuancore.com>
2018-04-18 21:59:02 +0800

    更改登录访问接口

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -5,7 +5,7 @@ const noProxy = process.env.NO_PROXY === 'false';
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 const proxy = {
   /* eslint-disable */
-  'POST /user/_login': 'http://112.74.99.173:80/ShareZone',
+  'POST /user/login': 'http://112.74.99.173:80/ShareZone',
 };
 
 export default noProxy ? {} : delay(proxy, 1000);
